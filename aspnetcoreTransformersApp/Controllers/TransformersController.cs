8e6b4cb7701898ff0dee6e6de4b1c30b74f8ed2c
Nilesh P <emailnileshp@gmail.com>
2019-04-12 22:29:49 -0400

    Added information logging

--- aspnetcoreTransformersApp/Controllers/TransformersController.cs
+++ aspnetcoreTransformersApp/Controllers/TransformersController.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using aspnetcoreTransformersApp.Models;
-using aspnetcoreTransformersApp.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace aspnetcoreTransformersApp.Controllers
 {
     [Route("api/[controller]")]
     public class TransformersController : Controller
     {
+        private readonly ILogger<Controller> _logger;
         private ITransformerAdd _transfromerAdd;
         private ITransformerRetrieve _transformerRetrieve;
         private ITransformerUpdate _transformerUpdate;
@@ -21,9 +17,10 @@ namespace aspnetcoreTransformersApp.Controllers
         private ITransformerScore _transformerScore;
         private ITransformerWar _transformerWar;
 
-        public TransformersController(ITransformerAdd transformerAdd, ITransformerRetrieve transformerRetrieve, ITransformerUpdate transformerUpdate
+        public TransformersController(ILogger<Controller> logger, ITransformerAdd transformerAdd, ITransformerRetrieve transformerRetrieve, ITransformerUpdate transformerUpdate
                 , ITransformerRemove transformerRemove, ITransformerList transformerList, ITransformerScore transformerScore, ITransformerWar transformerWar)
         {
+            _logger = logger;
             _transfromerAdd = transformerAdd;
             _transformerRetrieve = transformerRetrieve;
             _transformerUpdate = transformerUpdate;
@@ -41,6 +38,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpPost("[action]")]
         public async Task<IActionResult> Add([FromBody] Transformer transformer)
         {
+            _logger?.LogInformation("Add action called to add transformer");
             return await _transfromerAdd.ExecuteAdd(transformer);
         }
 
@@ -52,6 +50,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpGet("[action]/{transformerId}")]
         public async Task<IActionResult> Retrieve(int transformerId)
         {
+            _logger?.LogInformation("Retrive action called to retrieve transformer");
             return await _transformerRetrieve.ExecuteRetrieve(transformerId);
         }
 
@@ -64,6 +63,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpPut("[action]/{transformerId}")]
         public async Task<IActionResult> Update([FromBody] Transformer transformer, int transformerId)
         {
+            _logger?.LogInformation("Update action called to update transformer");
             return await _transformerUpdate.ExecuteUpdate(transformer, transformerId);
         }
 
@@ -75,6 +75,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpDelete("[action]/{transformerId}")]
         public async Task<IActionResult> Remove(int transformerId)
         {
+            _logger?.LogInformation("Remove action called to remove transformer");
             return await _transformerRemove.ExecuteRemove(transformerId);
         }
 
@@ -85,6 +86,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> AutobotSortedList()
         {
+            _logger?.LogInformation("AutobotSortedList action called to retrieve sorted list of autobots");
             return await _transformerList.ExecuteList(TransformersEnums.TransformerAllegiances.Autobot.ToString(), true);
         }
 
@@ -95,6 +97,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> DecepticonSortedList()
         {
+            _logger?.LogInformation("DecepticonSortedList action called to retrieve sorted list of decepticons");
             return await _transformerList.ExecuteList(TransformersEnums.TransformerAllegiances.Decepticon.ToString(), true);
         }
 
@@ -106,6 +109,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpGet("[action]/{transformerId}")]
         public async Task<IActionResult> Score(int transformerId)
         {
+            _logger?.LogInformation("Score action called to retrieve score of a transformer");
             return await _transformerScore.ExecuteScore(transformerId);
         }
 
@@ -116,6 +120,7 @@ namespace aspnetcoreTransformersApp.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> War()
         {
+            _logger?.LogInformation("War action called to simulate war between aubot and decepticon");
             return await _transformerWar.ExecuteWar();
         }
     }
